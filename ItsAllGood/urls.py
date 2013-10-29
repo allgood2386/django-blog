8d9f19f8c11b7c5c1688ef128214ddcb3587087f
Rich Allen <rallen@taoti.com>
2013-10-28 22:40:42 -0400

    https://docs.djangoproject.com/en/1.5/intro/tutorial04/ Use generic views: Less code is better

--- ItsAllGood/urls.py
+++ ItsAllGood/urls.py
@@ -7,5 +7,5 @@ urlpatterns = patterns(
     '',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^polls/', include('polls.urls'), namespace="polls"),
+    url(r'^polls/', include('polls.urls', namespace='polls')),
 )
